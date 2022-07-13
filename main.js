184e980a1a8935c3da1fcf712972001b8ab5acfb
grJDh <rusmukhametov@gmail.com>
2022-07-13 20:42:30 +0300

    I forgot about commits

--- main.js
+++ main.js
@@ -1,29 +1,99 @@
-applyTitle.addEventListener("click", async () => {
+//finding all inputs in popup
+const thumbnailInputElement = document.getElementById('thumbnailInput');
+const titleInputElement = document.getElementById('titleInput');
+const channelNameInputElement = document.getElementById('channelNameInput');
+const numInputElement = document.getElementById('numInput');
+
+//placing previous/default values from storage
+chrome.storage.local.get('thumbnailInputValue', result => {
+  if (result['thumbnailInputValue'] === undefined) thumbnailInputElement.defaultValue = "";
+  else thumbnailInputElement.defaultValue = result['thumbnailInputValue'];
+});
+chrome.storage.local.get('titleInputValue', result => {
+  if (result['titleInputValue'] === undefined)  titleInputElement.defaultValue = "";
+  else  titleInputElement.defaultValue = result['titleInputValue'];
+});
+chrome.storage.local.get('channelNameInputValue', result => {
+  if (result['channelNameInputValue'] === undefined) channelNameInputElement.defaultValue = "";
+  else channelNameInputElement.defaultValue = result['channelNameInputValue'];
+});
+chrome.storage.local.get('numInputValue', result => {
+  if (result['numInputValue'] === undefined) numInputElement.defaultValue = "1";
+  else numInputElement.defaultValue = result['numInputValue'];
+});
+
+//start listen to all changes to inputs and updating fake video
+thumbnailInput.addEventListener("input", async () => {
+  chrome.storage.local.set({"thumbnailInputValue": thumbnailInputElement.value}, () => {});
+  startScript();
+});
+titleInput.addEventListener("input", async () => {
+  chrome.storage.local.set({"titleInputValue": titleInputElement.value}, () => {});
+  startScript();
+});
+channelNameInput.addEventListener("input", async () => {
+  chrome.storage.local.set({"channelNameInputValue": channelNameInputElement.value}, () => {});
+  startScript();
+});
+numInput.addEventListener("input", async () => {
+  chrome.storage.local.set({"numInputValue": numInputElement.value}, () => {});
+  startScript();
+});
+
+// const saveOriginal
+
+//main function that is called for all changes
+const startScript = async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: applyNewTitle,
+    // args: [titleInputValue, channelNameInputValue, thumbnailInputValue],
+    function: applyChanges,
   });
-});
+}
 
-const applyNewTitle = () => {
-  const n = 0;
+//main function that is called for all changes
+startScript();
 
+//
+const applyChanges = async () => {
   if (window.location.href === 'https://www.youtube.com/') {
-    const primaryDiv = document.querySelectorAll("div.ytd-two-column-browse-results-renderer")[0];
+    
+  //getting values from storage
+    const returnValueFromStorage = async (key) => {
+      return new Promise((resolve, reject) => {
+        chrome.storage.local.get(key, function (result) {
+          if (result[key] === undefined) {
+            console.log("No value found");
+            reject();
+          } else {
+            resolve(result[key]);
+          }
+        });
+      });
+    };
 
-    const contentDiv = primaryDiv.children[0].children["contents"];
+    //index of video that will be replaced
+    const indexOfVideoToReplace = await returnValueFromStorage('numInputValue') - 1;
 
-    const rowDiv = contentDiv.children[n]
-    // [0] - первая строка, [1] - вторая, итд
+    //div with chosen video
+    const videoDiv = document.querySelectorAll("ytd-rich-grid-media")[indexOfVideoToReplace].children["dismissible"];
 
-    const videoDiv = rowDiv.children[0].children[n].children[0].children[0].children["dismissible"];
-    // [0] - первое видео, [1] - второе, итд
+    //getting avatar of the account in case of "Get avatar from account" checkbox
+    const avatarFromTopbar = document.querySelectorAll("#avatar-btn")[0].children[0].children[0];
 
-    const title = videoDiv.getElementsByTagName("h3")[0].getElementsByTagName("a")[0].children[0]; 
+    //finding all components of the video
+    const thumbnail = videoDiv.getElementsByTagName("yt-img-shadow")[0].children[0];
+    const title = videoDiv.getElementsByTagName("h3")[0].getElementsByTagName("a")[0].children[0];
+    const channelName = videoDiv.getElementsByTagName("ytd-channel-name")[0].children["container"].children["text-container"].children[0].children[0];
+    const avatar = videoDiv.children["details"].getElementsByTagName("a")[0].children[0].children[0];
 
-    title.textContent = "Right Title"
+    //applying all fake changes to video
+    thumbnail.src = await returnValueFromStorage('thumbnailInputValue');
+    title.textContent = await returnValueFromStorage('titleInputValue');
+    channelName.textContent = await returnValueFromStorage('channelNameInputValue');
+    avatar.src = avatarFromTopbar.src;
   }
 }
 
